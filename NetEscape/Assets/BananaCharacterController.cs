28298e762ae330cd05ebb61516bd8ce93b15cf4a
Bastian André <b.andre@neusta.de>
2020-11-07 01:21:57 +0100

    add basic character controller

---
+++ NetEscape/Assets/BananaCharacterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BananaCharacterController : MonoBehaviour
+{
+
+    public BaseCharacterController2D controller;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        // Move Banana
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
